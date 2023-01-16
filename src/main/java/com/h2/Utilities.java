862954d854daa4bf18d964ccd455ae68008da091
Tarunasaireddy <49843582+Tarunasaireddy@users.noreply.github.com>
2023-01-16 16:43:13 +0900

    Update Utilities.java

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -1,50 +1,34 @@
 package com.h2;
 
-import java.text.DecimalFormat;
+public class Utilities {
 
-public class MortgageCalculator {
-    private long loanAmount;
-    private int termInYears;
-    private float annualRate;
-    private double monthlyPayment;
-
-    public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
-        this.loanAmount = loanAmount;
-        this.termInYears = termInYears;
-        this.annualRate = annualRate;
+    public static long getLongValue(String in) {
+        long out = Long.MIN_VALUE;
+        try {
+            out = Long.parseLong(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
+        }
+        return out;
     }
 
-    private int getNumberOfPayments() {
-        return termInYears * 12;
+    public static int getIntValue(String in) {
+        int out = Integer.MIN_VALUE;
+        try {
+            out = Integer.parseInt(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
+        }
+        return out;
     }
 
-    private float getMonthlyInterestRate() {
-        float interestRate = annualRate / 100;
-        return interestRate / 12;
+    public static float getFloatValue(String in) {
+        float out = Float.MIN_VALUE;
+        try {
+            out = Float.parseFloat(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
+        }
+        return out;
     }
-
-    public void calculateMonthlyPayment() {
-        long P = loanAmount;
-        float r = getMonthlyInterestRate();
-        int n = getNumberOfPayments();
-
-        double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
-        this.monthlyPayment = M;
-    }
-
-    public String toString() {
-        DecimalFormat df = new DecimalFormat("####0.00");
-        return "monthlyPayment: " + df.format(monthlyPayment);
-    }
-
-    public static void main(String[] args) {
-        long loanAmount = Utilities.getLongValue(args[0]);
-        int termInYears = Utilities.getIntValue(args[1]);
-        float annualRate = Utilities.getFloatValue(args[2]);
-
-        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
-        calculator.calculateMonthlyPayment();
-        System.out.println(calculator.toString());
-    }
-
 }
